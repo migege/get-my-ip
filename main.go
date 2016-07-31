c17a2d2461489b1ac992e16ff46f76bd0970f303
lizhengwei <lizhengwei@panda.tv>
2016-07-31 23:04:46 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net"
+	"strings"
+)
+
+func handleConnection(conn net.Conn) {
+	addr := conn.RemoteAddr().String()
+	addr_parts := strings.Split(addr, ":")
+	ip := ""
+	if len(addr_parts) > 0 {
+		ip = addr_parts[0]
+	}
+	conn.Write([]byte(ip))
+	conn.Close()
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":6666")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+		}
+		go handleConnection(conn)
+	}
+}
